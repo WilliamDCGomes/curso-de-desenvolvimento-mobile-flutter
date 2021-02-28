1d89bfc1d7ae86f8ead9a68c7cd04b40d1220faf
WilliamDCGomes <williamdouglasgomes@hotmail.com>
2021-02-28 18:35:06 -0300

    Botao, linha e hierarquia

--- contador_de_pessoas/lib/main.dart
+++ contador_de_pessoas/lib/main.dart
@@ -9,6 +9,23 @@ void main() {
             Text("Pessoas: 0",
                 style: TextStyle(
                     color: Colors.white, fontWeight: FontWeight.bold)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                FlatButton(
+                  child: Text("+1",
+                    style: TextStyle(fontSize: 40.0, color: Colors.white),
+                  ),
+                  onPressed: () {},
+                ),
+                FlatButton(
+                  child: Text("-1",
+                    style: TextStyle(fontSize: 40.0, color: Colors.white),
+                  ),
+                  onPressed: () {},
+                ),
+              ],
+            ),
             Text("Pode Entrar",
                 style: TextStyle(
                     color: Colors.white,
