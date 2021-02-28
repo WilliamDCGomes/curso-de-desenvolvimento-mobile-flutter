ec8b7457bf14c60efb91ec003a0dd1950782d10b
WilliamDCGomes <williamdouglasgomes@hotmail.com>
2021-02-28 18:20:49 -0300

    Coluna e texto

--- contador_de_pessoas/lib/main.dart
+++ contador_de_pessoas/lib/main.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/material.dart';
-void main(){
+
+void main() {
   runApp(MaterialApp(
-    title: "Contadoer de Pessoas",
-    home: Container(color: Colors.white,)
-  ));
+      title: "Contadoer de Pessoas",
+      home: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text("Pessoas: 0",
+                style: TextStyle(
+                    color: Colors.white, fontWeight: FontWeight.bold)),
+            Text("Pode Entrar",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontStyle: FontStyle.italic,
+                    fontSize: 30.0))
+          ])));
 }
