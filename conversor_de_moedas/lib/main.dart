0ea8ef9014de17a7a1361d3cf935f52f1472cea7
WilliamDCGomes <williamdouglasgomes@hotmail.com>
2021-03-09 17:01:34 -0300

    Comecando a trabalhar com FutureBuilder

--- conversor_de_moedas/lib/main.dart
+++ conversor_de_moedas/lib/main.dart
@@ -1,14 +1,36 @@
 import 'package:flutter/material.dart';
 import "package:http/http.dart" as http;
-import "package:async/async.dart";
+import "dart:async";
 import "dart:convert";
 
 const request = "https://api.hgbrasil.com/finance?format=json&key=dd81bc40";
 void main() async {
-  http.Response response = await http.get(request);
-  json.decode(response.body);
   
   runApp(MaterialApp(
     home: Container()
   ));
+}
+Future<Map> getData() async{
+  http.Response response = await http.get(request);
+  return json.decode(response.body);
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: Text("\$ Conversor \$",),
+        backgroundColor: Colors.amber,
+        centerTitle: true,
+      ),
+      body: ,
+    );
+  }
 }
