b908f30f655da7214b0887dac7925dccd9baacce
WilliamDCGomes <williamdouglasgomes@hotmail.com>
2021-03-18 22:17:59 -0300

    Adicionando grid de gifs

--- buscador_gifs/lib/ui/home_page.dart
+++ buscador_gifs/lib/ui/home_page.dart
@@ -77,6 +77,21 @@ class _HomePageState extends State<HomePage> {
     );
   }
   Widget _createGifTable(BuildContext context, AsyncSnapshot snapshot){
-    return null;
+    return GridView.builder(
+        padding: EdgeInsets.all(10.0),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 10.0,
+          mainAxisSpacing: 10.0
+        ),
+        itemCount: snapshot.data["data"].length,
+        itemBuilder: (context, index){
+          return GestureDetector(
+            child: Image.network(snapshot.data["data"][index]["images"]["fixed_height"]["url"],
+            height: 300.0,
+            fit: BoxFit.cover,),
+          );
+        }
+    );
   }
 }
