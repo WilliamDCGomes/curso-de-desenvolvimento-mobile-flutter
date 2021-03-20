0f74d4a3a5abf9736049a44bc6f4895573c45d90
WilliamDCGomes <williamdouglasgomes@hotmail.com>
2021-03-20 19:19:19 -0300

    Criando tela para visualizar gif separado

--- buscador_gifs/lib/ui/home_page.dart
+++ buscador_gifs/lib/ui/home_page.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:buscador_gifs/ui/gif_page.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -108,6 +109,11 @@ class _HomePageState extends State<HomePage> {
                 snapshot.data["data"][index]["images"]["fixed_height"]["url"],
                 height: 300.0,
                 fit: BoxFit.cover,),
+              onTap: (){
+                Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => GifPage(snapshot.data["data"][index]))
+                );
+              },
             );
           }
           else{
