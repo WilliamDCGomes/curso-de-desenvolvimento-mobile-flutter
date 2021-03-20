7d1952a3a62dcb39dacd23d8e9740a1fec21fab0
WilliamDCGomes <williamdouglasgomes@hotmail.com>
2021-03-20 19:43:27 -0300

    Implementado funcao de compartilhar gifs

--- buscador_gifs/lib/ui/gif_page.dart
+++ buscador_gifs/lib/ui/gif_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:share/share.dart';
 
 class GifPage extends StatelessWidget {
   final Map _gifData;
@@ -10,6 +11,15 @@ class GifPage extends StatelessWidget {
       appBar: AppBar(
         title: Text(_gifData["title"]),
         backgroundColor: Colors.black,
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(Icons.share),
+              onPressed: (){
+                Share.share(_gifData["images"]["fixed_height"]["url"]);
+              }
+          )
+
+        ],
       ),
       body: Center(
         child: Image.network(_gifData["images"]["fixed_height"]["url"]),
--- buscador_gifs/lib/ui/home_page.dart
+++ buscador_gifs/lib/ui/home_page.dart
@@ -1,5 +1,5 @@
 import 'dart:convert';
-
+import 'package:share/share.dart';
 import 'package:buscador_gifs/ui/gif_page.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -113,6 +113,9 @@ class _HomePageState extends State<HomePage> {
                 Navigator.push(context,
                   MaterialPageRoute(builder: (context) => GifPage(snapshot.data["data"][index]))
                 );
+              },
+              onLongPress: (){
+                Share.share(snapshot.data["data"][index]["images"]["fixed_height"]["url"]);
               },
             );
           }
