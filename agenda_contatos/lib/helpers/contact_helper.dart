2509e17ea4ca58a7d249ff31f737f15c873e311b
WilliamDCGomes <williamdouglasgomes@hotmail.com>
2021-03-25 17:52:18 -0300

    Inicializando o banco de dados

--- agenda_contatos/lib/helpers/contact_helper.dart
+++ agenda_contatos/lib/helpers/contact_helper.dart
@@ -1,5 +1,7 @@
 import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
 
+final String contactTable = "contactTable";
 final String idColumn = "idColumn";
 final String nameColumn = "nameColumn";
 final String emailColumn = "emailColumn";
@@ -7,6 +9,28 @@ final String phoneColumn = "phoneColumn";
 final String imgColumn = "imgcolumn";
 
 class ContactHelper{
+  static final ContactHelper _intance = new ContactHelper.internal();
+  factory ContactHelper() => _intance;
+  ContactHelper.internal();
+  Database _db;
+  Future<Database> get db async{
+    id(_db != null){
+      return _db;
+    }
+    else{
+      _db = await initDb();
+      return _db;
+    }
+  }
+  Future<Database> initDb() async{
+    final databasesPath = await getDatabasesPath();
+    final path = join(databasesPath, "contacts.db");
+    return await openDatabase(path, version: 1, onCreate: (Database db, int newerVersion) async {
+      await db.execute(
+        "CREATE TABLE $contactTable($idColumn INTEGER PRIMARY KEY, $nameColumn TEXT, $emailColumn TEXT, $phoneColumn TEXT, $imgColumn TEXT)"
+      );
+    });
+  }
 
 }
 
