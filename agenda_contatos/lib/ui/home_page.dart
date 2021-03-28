93fa18018d157f10a95e6fc6a0edba5ac2734515
WilliamDCGomes <williamdouglasgomes@hotmail.com>
2021-03-28 17:26:38 -0300

    Passando contatos entre as telas

--- agenda_contatos/lib/ui/home_page.dart
+++ agenda_contatos/lib/ui/home_page.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:agenda_contatos/helpers/contact_helper.dart';
+import 'package:agenda_contatos/ui/contact_page.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -15,11 +16,7 @@ class _HomePageState extends State<HomePage> {
   @override
   void initState() {
     super.initState();
-    helper.getAllcontacts().then((list){
-      setState(() {
-        contacts = list;
-      });
-    });
+    _getAllContacts();
   }
 
   @override
@@ -32,7 +29,9 @@ class _HomePageState extends State<HomePage> {
       ),
       backgroundColor: Colors.white,
       floatingActionButton: FloatingActionButton(
-        onPressed: (){},
+        onPressed: (){
+          _showContactPage();
+        },
         child: Icon(Icons.add),
         backgroundColor: Colors.deepPurple,
       ),
@@ -89,6 +88,32 @@ class _HomePageState extends State<HomePage> {
           ),
         ),
       ),
+      onTap: (){
+        _showContactPage(contact: contacts[index]);
+      },
     );
+  }
+  void _showContactPage({Contact contact}) async {
+    final recContact = await Navigator.push(
+        context, MaterialPageRoute(
+          builder: (context)=>ContactPage(contact: contact,)
+        )
+    );
+    if(recContact != null){
+      if(contact != null){
+        await helper.updateContact(recContact);
+      }
+      else{
+        await helper.saveContact(recContact);
+      }
+      _getAllContacts();
+    }
+  }
+  void _getAllContacts(){
+    helper.getAllcontacts().then((list){
+      setState(() {
+        contacts = list;
+      });
+    });
   }
 }
