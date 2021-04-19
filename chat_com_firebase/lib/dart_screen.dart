9517c1aac9e64b5160dcec4a5043c9f349db7d07
WilliamDCGomes <williamdouglasgomes@hotmail.com>
2021-04-19 18:36:02 -0300

    Enviando texto para o firebase

--- chat_com_firebase/lib/dart_screen.dart
+++ chat_com_firebase/lib/dart_screen.dart
@@ -1,4 +1,5 @@
 import 'package:chat_com_firebase/text_composer.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatefulWidget {
@@ -7,6 +8,11 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  void _sendMessage(String text){
+    Firestore.instance.collection('messages').add({
+      'text' : text
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,7 +20,7 @@ class _ChatScreenState extends State<ChatScreen> {
         title: Text("Hello"),
         elevation: 0,
       ),
-      body: TextComposer(),
+      body: TextComposer(_sendMessage),
     );
   }
 }
--- chat_com_firebase/lib/text_composer.dart
+++ chat_com_firebase/lib/text_composer.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
 
 class TextComposer extends StatefulWidget {
+  TextComposer(this.sendMessage);
+  Function(String) sendMessage;
   @override
   _TextComposerState createState() => _TextComposerState();
 }
 
 class _TextComposerState extends State<TextComposer> {
+  final TextEditingController _controller = TextEditingController();
   bool _isComposing = false;
+  void _reset(){
+    _controller.clear();
+    setState(() {
+      _isComposing = false;
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -21,6 +30,7 @@ class _TextComposerState extends State<TextComposer> {
           ),
           Expanded(
               child: TextField(
+                controller: _controller,
                 decoration: InputDecoration.collapsed(hintText: "Enviar uma Mensagem"),
                 onChanged: (text){
                   setState(() {
@@ -28,14 +38,16 @@ class _TextComposerState extends State<TextComposer> {
                   });
                 },
                 onSubmitted: (text){
-
+                  widget.sendMessage(text);
+                  _reset();
                 },
               )
           ),
           IconButton(
               icon: Icon(Icons.send),
               onPressed: _isComposing ? (){
-
+                widget.sendMessage(_controller.text);
+                _reset();
               } : null,
           )
         ],
