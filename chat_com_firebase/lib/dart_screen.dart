4d45b0568bcb589a7e3c5d61859c0aa1303ab881
WilliamDCGomes <williamdouglasgomes@hotmail.com>
2021-04-19 18:11:29 -0300

    Criando campos de digitação

---
+++ chat_com_firebase/lib/dart_screen.dart
@@ -0,0 +1,20 @@
+import 'package:chat_com_firebase/text_composer.dart';
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatefulWidget {
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Hello"),
+        elevation: 0,
+      ),
+      body: TextComposer(),
+    );
+  }
+}
--- chat_com_firebase/lib/main.dart
+++ chat_com_firebase/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:chat_com_firebase/dart_screen.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
@@ -10,14 +11,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Chat Flutter',
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
+        iconTheme: IconThemeData(
+          color: Colors.blue
+        )
       ),
-      home: Container(
-
-      ),
+      home: ChatScreen(),
     );
   }
 }
