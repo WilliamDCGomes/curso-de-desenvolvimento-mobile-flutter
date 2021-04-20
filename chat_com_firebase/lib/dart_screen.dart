1a1026b0b284dafcb677ea1cbf6120e58dc75451
WilliamDCGomes <williamdouglasgomes@hotmail.com>
2021-04-19 22:35:18 -0300

    Inserido função para exibir lista de mensagens

--- chat_com_firebase/lib/dart_screen.dart
+++ chat_com_firebase/lib/dart_screen.dart
@@ -32,7 +32,36 @@ class _ChatScreenState extends State<ChatScreen> {
         title: Text("Hello"),
         elevation: 0,
       ),
-      body: TextComposer(_sendMessage),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+              child: StreamBuilder<QuerySnapshot>(
+                stream: Firestore.instance.collection('messages').snapshots(),
+                builder: (context, snapshot){
+                  switch(snapshot.connectionState){
+                    case ConnectionState.none:
+                    case ConnectionState.waiting:
+                      return Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    default:
+                      List<DocumentSnapshot> documents = snapshot.data.documents.reversed.toList();
+                      return ListView.builder(
+                        itemCount: documents.length,
+                          reverse: true,
+                          itemBuilder: (context, index){
+                            return ListTile(
+                              title: Text(documents[index].data['text']),
+                            );
+                          }
+                      );
+                  }
+                },
+              )
+          ),
+          TextComposer(_sendMessage),
+        ]
+      )
     );
   }
 }
