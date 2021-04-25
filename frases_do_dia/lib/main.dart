b2f5209fc55fc9abe7e7a27cc50aea7fef4d71ab
WilliamDCGomes <williamdouglasgomes@hotmail.com>
2021-04-25 19:23:10 -0300

    Criando interface

--- frases_do_dia/lib/main.dart
+++ frases_do_dia/lib/main.dart
@@ -3,43 +3,55 @@ import 'package:flutter/material.dart';
 void main(){
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: Scaffold(
+    home: Home(),
+  ));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  String _frasesDoDia = "Gratidão não é pagamento, mas um reconhecimento que se demonstra no dia a dia";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
-        title: Text("Instagram"),
+        title: Text(
+          "Frases do Dia"
+        ),
         backgroundColor: Colors.green,
       ),
-      body: Padding(
-        padding: EdgeInsets.all(16),
+      body: Container(
+        padding: EdgeInsets.all(15),
+        width: double.infinity,
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-            Text("Conteúdo principal"),
-            FlatButton(
-                onPressed: (){},
-                child: Text("Teste")
+            Image.asset("images/logo.png"),
+            Text(
+                _frasesDoDia,
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 20,
+                  fontStyle: FontStyle.italic,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.justify,
             ),
             RaisedButton(
                 onPressed: (){},
-                child: Text("Teste"),
-            ),
-            FloatingActionButton(
-                onPressed: (){},
-                child: Text("+")
+                color: Colors.green,
+                child: Text(
+                  "Nova Frase",
+                  style: TextStyle(color: Colors.white),
+                ),
             )
           ],
-        )
-      ),
-      bottomNavigationBar: BottomAppBar(
-        color: Colors.green,
-        child: Padding(
-          padding: EdgeInsets.all(16),
-          child: Row(
-            children: <Widget>[
-              Text("Conteúdo principal"),
-              Text("Conteúdo principal"),
-            ],
-          ),
         ),
       ),
-    ),
-  ));
+    );
+  }
 }
