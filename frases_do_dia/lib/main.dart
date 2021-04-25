7e9581ce1529585d036acb86c098825b289b764e
WilliamDCGomes <williamdouglasgomes@hotmail.com>
2021-04-25 17:00:42 -0300

    Botões disponíveis no flutter

--- frases_do_dia/lib/main.dart
+++ frases_do_dia/lib/main.dart
@@ -2,20 +2,44 @@ import 'package:flutter/material.dart';
 
 void main(){
   runApp(MaterialApp(
-    title: "Frases do dia",
-    home: Container(
-      padding: EdgeInsets.all(30),
-      decoration: BoxDecoration(
-        border: Border.all(
-            width: 3,
-            color: Colors.white
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("Instagram"),
+        backgroundColor: Colors.green,
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          children: <Widget>[
+            Text("Conteúdo principal"),
+            FlatButton(
+                onPressed: (){},
+                child: Text("Teste")
+            ),
+            RaisedButton(
+                onPressed: (){},
+                child: Text("Teste"),
+            ),
+            FloatingActionButton(
+                onPressed: (){},
+                child: Text("+")
+            )
+          ],
         )
       ),
-      child: Column(
-        children: <Widget>[
-          Text("Texto")
-        ],
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.green,
+        child: Padding(
+          padding: EdgeInsets.all(16),
+          child: Row(
+            children: <Widget>[
+              Text("Conteúdo principal"),
+              Text("Conteúdo principal"),
+            ],
+          ),
+        ),
       ),
-    )
+    ),
   ));
 }
