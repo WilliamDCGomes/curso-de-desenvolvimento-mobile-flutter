1e6e7c3a2d58c6f59048a51c17a681421b957ca9
WilliamDCGomes <williamdouglasgomes@hotmail.com>
2021-05-31 19:24:19 -0300

    Criado aplicativo de cara e coroa

---
+++ cara_ou_coroa/lib/ResultCoin.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+class SecondScreen extends StatefulWidget {
+  String imageResult;
+  SecondScreen(this.imageResult);
+
+  @override
+  _SecondScreenState createState() => _SecondScreenState();
+}
+
+class _SecondScreenState extends State<SecondScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff70c393),
+      body: Container(
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Image.asset(widget.imageResult, alignment: Alignment.center),
+            Padding(
+                padding: EdgeInsets.only(top: 40.0),
+                child: GestureDetector(
+                  onTap: (){
+                    Navigator.pop(context);
+                  },
+                  child: Image.asset("images/botao_voltar.png", alignment: Alignment.center),
+                )
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ cara_ou_coroa/lib/main.dart
@@ -0,0 +1,55 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+import 'ResultCoin.dart';
+void main(){
+  runApp(
+      MaterialApp(
+        home: Home(),
+        debugShowCheckedModeBanner: false,
+      )
+  );
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  void playCoin(){
+    int result = Random().nextInt(2);
+    String imageResult = result == 0 ? "images/moeda_cara.png" : "images/moeda_coroa.png";
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => SecondScreen(imageResult)
+        )
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff70c393),
+      body: Container(
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Image.asset("images/logo.png", alignment: Alignment.center),
+            Padding(
+                padding: EdgeInsets.only(top: 40.0),
+                child: GestureDetector(
+                  onTap: playCoin,
+                  child: Image.asset("images/botao_jogar.png", alignment: Alignment.center),
+                )
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
