6a1acd939f94009805bcacf9505a83ec63c4011b
WilliamDCGomes <williamdouglasgomes@hotmail.com>
2021-06-13 20:22:32 -0300

    Utilizando Put, Patch e Delete

--- requisicoes_post/lib/Home.dart
+++ requisicoes_post/lib/Home.dart
@@ -42,16 +42,50 @@ class _HomeState extends State<Home> {
     print("resposta: ${response.body}");
   }
 
-  _put(){
-
+  _put() async {
+    var body = json.encode(
+        {
+          "userId": 42,
+          "id": null,
+          "title": "Teste Will Flutter 2021 alterando para teste",
+          "body": "Corpo da Postagem alterado"
+        }
+    );
+    http.Response response = await http.put(
+        urlBase + "/posts/2",
+        headers: {
+          "Content-type": "application/json; charset=UTF-8"
+        },
+        body: body
+    );
+    print("resposta: ${response.statusCode}");
+    print("resposta: ${response.body}");
   }
 
-  _patch(){
-
+  _patch() async {
+    var body = json.encode(
+        {
+          "userId": 42,
+          "body": "Patch pode alterar apenas alguns campos"
+        }
+    );
+    http.Response response = await http.patch(
+        urlBase + "/posts/2",
+        headers: {
+          "Content-type": "application/json; charset=UTF-8"
+        },
+        body: body
+    );
+    print("resposta: ${response.statusCode}");
+    print("resposta: ${response.body}");
   }
 
-  _delete(){
-
+  _delete() async {
+    http.Response response = await http.delete(
+        urlBase + "/posts/2"
+    );
+    print("resposta: ${response.statusCode}");
+    print("resposta: ${response.body}");
   }
 
   Widget GetFuture(){
@@ -121,11 +155,11 @@ class _HomeState extends State<Home> {
                     ),
                 ),
                 Padding(
-                    padding: EdgeInsets.only(left: 10.0, right: 10.0),
+                    padding: EdgeInsets.only(left: 10.0),
                     child: FlatButton(
-                      onPressed: _post,
+                      onPressed: _put,
                       child: Text(
-                        "Atualizar",
+                        "Atualizar Tudo",
                         style: TextStyle(
                             color: Colors.white
                         ),
@@ -136,8 +170,24 @@ class _HomeState extends State<Home> {
                       ),
                     ),
                 ),
+                Padding(
+                    padding: EdgeInsets.only(left: 10.0, right: 10.0),
+                    child: FlatButton(
+                      onPressed: _patch,
+                      child: Text(
+                        "Atualizar algumas coisas",
+                        style: TextStyle(
+                            color: Colors.white
+                        ),
+                      ),
+                      color: Colors.orange,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15)
+                      ),
+                    ),
+                ),
                 FlatButton(
-                    onPressed: _post,
+                    onPressed: _delete,
                     child: Text(
                       "Remover",
                       style: TextStyle(
