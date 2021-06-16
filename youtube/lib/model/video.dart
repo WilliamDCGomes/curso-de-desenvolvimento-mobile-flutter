86772bc265cf1d439f8490edf9614b73ca35a3e0
WilliamDCGomes <williamdouglasgomes@hotmail.com>
2021-06-16 19:47:29 -0300

    Convertendo resultados

--- youtube/lib/model/video.dart
+++ youtube/lib/model/video.dart
@@ -1,9 +1,17 @@
 class Video{
   String id;
   String title;
-  String description;
   String image;
   String channel;
 
-  Video({this.id, this.title, this.description, this.image, this.channel});
+  Video({this.id, this.title, this.image, this.channel});
+
+  factory Video.fromJson(Map<String, dynamic> json){
+    return Video(
+      id: json["id"]["videoId"],
+      title: json["snippet"]["title"],
+      image: json["snippet"]["thumbnails"]["high"]["url"],
+      channel: json["snippet"]["channelId"],
+    );
+  }
 }
