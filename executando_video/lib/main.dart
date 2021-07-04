0c486be5547f3f61af6c29ab407a65f1ac518c73
WilliamDCGomes <williamdouglasgomes@hotmail.com>
2021-07-04 20:39:18 -0300

    Configurando primeira parte do app

--- executando_video/lib/main.dart
+++ executando_video/lib/main.dart
@@ -1,8 +1,34 @@
 import 'package:flutter/material.dart';
-
+import 'package:video_player/video_player.dart';
 void main() {
   runApp(MaterialApp(
     home: Home(),
     debugShowCheckedModeBanner: false,
   ));
+}
+class Home extends StatefulWidget {
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  VideoPlayerController videoPlayerController;
+
+  @override
+  void initState() {
+    super.initState();
+    videoPlayerController = VideoPlayerController.network(
+      "https://www.youtube.com/watch?v=j2K1Dr3LIN8&ab_channel=CanalPeeWee"
+    )..initialize().then((_){
+      setState(() {
+
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
 }
