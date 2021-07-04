90fbafd5330569d8eb35077d85efd0ae6d3a561f
WilliamDCGomes <williamdouglasgomes@hotmail.com>
2021-07-04 20:00:19 -0300

    Criando interface com Grid

--- aprenda_ingles/lib/screens/bichos.dart
+++ aprenda_ingles/lib/screens/bichos.dart
@@ -9,6 +9,47 @@ class Bichos extends StatefulWidget {
 class _BichosState extends State<Bichos> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return GridView.count(
+      crossAxisCount: 2,
+      childAspectRatio: MediaQuery.of(context).size.aspectRatio * 2,
+      children: <Widget>[
+        GestureDetector(
+          onTap: (){
+
+          },
+          child: Image.asset("assets/images/cao.png"),
+        ),
+        GestureDetector(
+          onTap: (){
+
+          },
+          child: Image.asset("assets/images/gato.png"),
+        ),
+        GestureDetector(
+          onTap: (){
+
+          },
+          child: Image.asset("assets/images/leao.png"),
+        ),
+        GestureDetector(
+          onTap: (){
+
+          },
+          child: Image.asset("assets/images/macaco.png"),
+        ),
+        GestureDetector(
+          onTap: (){
+
+          },
+          child: Image.asset("assets/images/ovelha.png"),
+        ),
+        GestureDetector(
+          onTap: (){
+
+          },
+          child: Image.asset("assets/images/vaca.png"),
+        ),
+      ],
+    );
   }
 }
