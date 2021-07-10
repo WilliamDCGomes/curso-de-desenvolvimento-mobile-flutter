93e4f2b63d0f2eedf6fed659470da8b02b9b65b2
WilliamDCGomes <williamdouglasgomes@hotmail.com>
2021-07-10 20:58:52 -0300

    Salvando e atualizando dados com firebase

--- firebase/lib/main.dart
+++ firebase/lib/main.dart
@@ -1,11 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  Firestore.instance.collection("usuarios").document("pontuacao").setData({
-    "William": "1000",
-    "Ana": "590"
-  });
+  Firestore db = Firestore.instance;
+  /*
+  db.collection("usuarios").document("002").setData(
+      {
+        "nome": "Ana Maria",
+        "idade": "20"
+      }
+  );
+  *//*
+  DocumentReference ref = await db.collection("noticias").add(
+    {
+      "titulo": "Novo planeta é descoberto",
+      "descricao": "Texto de exemplo...."
+    }
+  );
+
+  print("id salvo " + ref.documentID);
+  */
+  db.collection("noticias").document("xaoeZxJhS4HTAWjnRpFZ").setData(
+      {
+        "titulo": "Novo planeta é descoberto Alterado",
+        "descricao": "Texto de exemplo...."
+      }
+  );
+
   runApp(MaterialApp(
     home: Home(),
     debugShowCheckedModeBanner: false,
