45428f05b01ce6bf074f215048420df7c1389ffe
WilliamDCGomes <williamdouglasgomes@hotmail.com>
2021-07-28 21:32:35 -0300

    Configurando o projeto

---
+++ whatsapp/lib/Home.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class Home extends StatefulWidget {
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
