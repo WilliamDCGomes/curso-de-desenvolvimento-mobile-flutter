f729b566f3351741567541bad85f89430a69e128
WilliamDCGomes <williamdouglasgomes@hotmail.com>
2022-01-13 19:54:30 -0300

    Adicionando lista de conversa

--- whatsapp/lib/telas/AbaConversas.dart
+++ whatsapp/lib/telas/AbaConversas.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_sizer/responsive_sizer.dart';
 
 class AbaConversas extends StatefulWidget {
   const AbaConversas({Key? key}) : super(key: key);
@@ -8,15 +9,44 @@ class AbaConversas extends StatefulWidget {
 }
 
 class _AbaConversasState extends State<AbaConversas> {
+  bool _hasPicture = false;
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        "Conversas",
-        style: TextStyle(
-          color: Colors.white,
-        ),
-      ),
+    return ListView.builder(
+      itemCount: 5,
+      itemBuilder: (context, index){
+        return Padding(
+          padding: EdgeInsets.all(2.h),
+          child: Row(
+            children: [
+              Container(
+                height: 8.h,
+                width: 8.h,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(4.h),
+                  color: Color(0XFF096A63),
+                ),
+                child: _hasPicture ?
+                  Image.asset(
+                      ""
+                  ) :
+                  Center(
+                    child: Text(
+                      "WD",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 22.sp,
+                      ),
+                      textAlign: TextAlign.start,
+                    ),
+                  ),
+              ),
+            ],
+          ),
+        );
+      },
     );
   }
 }
